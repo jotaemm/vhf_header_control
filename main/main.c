a1ccb45a3052f8273fe7d5bb242be1d013a62508
jotaemm <jorgemonzonmonedero@gmail.com>
2023-05-03 09:37:43 +0100

    gpiodefinition

--- main/main.c
+++ main/main.c
@@ -1,6 +1,6 @@
 /*
- *  description: ***********
- *  author: Jorge Monzon Monedero
+ *  description: This code aims to control the performance of and VHF header. The components which are going to be managed are a variable attenuator
+ *  and the switching structure which selects the receiving or the transmitting branches.
  *  date:   27/04/2023
  */
 
@@ -19,50 +19,59 @@
 #include "string.h"
 
 /** Macros section **/
-#define UART0_TXD       1
-#define UART0_RXD       3
-#define UART0_RTS       -1
-#define UART0_CTS       -1
-#define UART0_PORT      0
-#define U0BAUD_RATE     115200
-#define BUFFER_SIZE     1024     
-#define GPIO_OUTPUT_IO_0    GPIO_NUM_2
-#define GPIO_OUTPUT_PIN_SEL  1ULL<<GPIO_OUTPUT_IO_0
+#define UART0_TXD       1                               // TXD buffer is in the GPIO1
+#define UART0_RXD       3                               // RXD buffer is in the GPIO3
+#define UART0_RTS       -1                              // Not configured
+#define UART0_CTS       -1                              // Not configured
+#define UART0_PORT      0                               // The UART used for this aplication - UART0
+#define U0BAUD_RATE     115200                          // Baudrate for the serial communications throught UART0
+#define BUFFER_SIZE     1024                            // Buffer size for the memory allocation
+/*GPIO pin used to set the variable attenuator*/
+#define GPIO_OUTPUT_32    GPIO_NUM_32
+#define GPIO_OUTPUT_33    GPIO_NUM_33
+#define GPIO_OUTPUT_25    GPIO_NUM_25
+#define GPIO_OUTPUT_26    GPIO_NUM_26
+#define GPIO_OUTPUT_27    GPIO_NUM_27
+#define GPIO_OUTPUT_14    GPIO_NUM_14
+#define GPIO_OUTPUT_12    GPIO_NUM_12
+#define GPIO_OUTPUT_13    GPIO_NUM_13      
+/* Bit mask for the pins which are going to use as outputs*/       
+#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_32)|(1ULL<<GPIO_OUTPUT_33)|(1ULL<<GPIO_OUTPUT_25)|(1ULL<<GPIO_OUTPUT_26)|(1ULL<<GPIO_OUTPUT_27)|(1ULL<<GPIO_OUTPUT_14)|(1ULL<<GPIO_OUTPUT_12)|(1ULL<<GPIO_OUTPUT_13))
 
 /** Global variables section**/
 static uart_config_t uart_config;
 static gpio_config_t io_conf = {};
-static uint8_t att_value = 0;
-//static QueueHandle_t uart0_queue;
-//static const char *TAG = "UART TEST";
 
 /** Prototypes section**/
 void configure_uart();
 void configure_gpio();
-//void menu();
-//int sendData(const char* logName, const char* data);
-//void echo_uart();
+void gpio_event(uint8_t *val);
+void menu();
+
 /** Task section**/
 static void uart_rcv_event_task(void *pvParameters);
-//static void uart_tx_event_task(void *arg);
-static void gpio_event_task(uint8_t *val);
 
 /** Main section**/
 void app_main(void)
 {
     configure_uart(); 
     configure_gpio();
+    menu();
     xTaskCreate(uart_rcv_event_task, "uart_rx_task", 1024*2, NULL, configMAX_PRIORITIES, NULL);
-    //xTaskCreate(uart_tx_event_task, "uart_tx_task", 1024*2, NULL, configMAX_PRIORITIES-1, NULL);
-    xTaskCreate(gpio_event_task, "gpio_task", 1024*2, NULL, configMAX_PRIORITIES-1, NULL);
 }
 /** Functions definitions**/
+void menu(){
+    static const char *menu = "\r\n1- xdB\n2- ydB\n3- zdB";
+    int len = strlen(menu);
+    uart_write_bytes(UART0_PORT,menu,len);
+}
+ 
 void configure_gpio(){
     //disable interrupt
     io_conf.intr_type = GPIO_INTR_DISABLE;
     //set as output mode
     io_conf.mode = GPIO_MODE_OUTPUT;
-    //bit mask of the pins that you want to set,e.g.GPIO18/19
+    //bit mask of the pins that you want to set
     io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
     //disable pull-down mode
     io_conf.pull_down_en = 0;
@@ -72,20 +81,17 @@ void configure_gpio(){
     gpio_config(&io_conf);
 }
 
-static void gpio_event_task(){
-    while (1)
+void gpio_event(uint8_t *val){
+    if (*val == '1')
     {
-        if (att_value == '1')
-        {
-            gpio_set_level(GPIO_OUTPUT_IO_0, 1);
-        } else if(att_value == '0'){
-            gpio_set_level(GPIO_OUTPUT_IO_0, 0);
-        }
-        vTaskDelay(10 / portTICK_PERIOD_MS);
+        gpio_set_level(GPIO_OUTPUT_32, 1);
+    } else if(*val == '0'){
+         gpio_set_level(GPIO_OUTPUT_32, 0);
     }
 }
 
-void configure_uart(){        
+void configure_uart(){  
+    /* Set the UART configuration*/      
     uart_config.baud_rate = U0BAUD_RATE;
     uart_config.data_bits = UART_DATA_8_BITS;
     uart_config.parity    = UART_PARITY_DISABLE;
@@ -96,41 +102,25 @@ void configure_uart(){
     #if CONFIG_UART_ISR_IN_IRAM
         intr_alloc_flags = ESP_INTR_FLAG_IRAM;
     #endif
+    /* Apply the configuration*/
     ESP_ERROR_CHECK(uart_driver_install(UART0_PORT, BUFFER_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
     ESP_ERROR_CHECK(uart_param_config(UART0_PORT, &uart_config));
     ESP_ERROR_CHECK(uart_set_pin(UART0_PORT, UART0_TXD, UART0_RXD, UART0_RTS, UART0_CTS));
 }
 
 static void uart_rcv_event_task(void *pvParameters){
-    uint8_t *data = (uint8_t *) malloc(BUFFER_SIZE);
+    uint8_t *data = (uint8_t *) malloc(BUFFER_SIZE);    // Allocate memory to variable data
     while (1)
-    {
+    {   
+        // Read data from the RXD buffer 
         const int data_len = uart_read_bytes(UART0_PORT, data, (BUFFER_SIZE - 1), 20 / portTICK_PERIOD_MS);
         if (data_len > 0)
-        {
-            uart_write_bytes(UART0_PORT, data, data_len);
-            att_value = *data;
+        {   
+            uart_write_bytes(UART0_PORT, data, data_len);   // Make an echo of the data received
+            gpio_event(data);                               // Set the attenuator configuration                  
             data[data_len] = '\0'; 
         }
         
     }
-    free(data);
+    free(data); // Free up the memory space assigned to variable data
 }
-/*
-static void uart_tx_event_task(void *arg)
-{
-    static const char *TX_TASK_TAG = "TX_TASK";
-    static const char *TX_TASK_MENU = "\nIntroduce a value for the attenuador\n";
-    //while (1) {
-        sendData(TX_TASK_TAG, TX_TASK_MENU);
-       // vTaskDelay(2000 / portTICK_PERIOD_MS);
-    //}
-}
-
-int sendData(const char* logName, const char* data)
-{
-    const int len = strlen(data);
-    const int txBytes = uart_write_bytes(UART0_PORT, data, len);
-    ESP_LOGI(logName, "Wrote %d bytes", txBytes);
-    return txBytes;
-}*/
